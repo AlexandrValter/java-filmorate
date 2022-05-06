acea779625e06ef1e03cff0289fd1ef3c5ae1f93
AlexandrValter <valteralexander@yandex.ru>
2022-05-06 21:27:42 +0700

    feat: добавлена валидация  с использованием spring-boot-starter-validation

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +18,7 @@ public class FilmController {
     private Integer id;
 
     @PostMapping
-    public Film addFilm(@RequestBody Film film) {
+    public Film addFilm(@Valid @RequestBody Film film) {
         if (validateFilm(film)) {
             film.setId(makeId());
             films.put(film.getId(), film);
@@ -33,7 +34,7 @@ public class FilmController {
     }
 
     @PutMapping
-    public void addOrUpdateFilm(@RequestBody Film film) {
+    public void addOrUpdateFilm(@Valid @RequestBody Film film) {
         if (validateFilm(film)) {
             if (film.getId() != null) {
                 if (films.containsKey(film.getId())) {
@@ -49,7 +50,7 @@ public class FilmController {
     }
 
     @PatchMapping
-    public void updateFilm(@RequestBody Film film) {
+    public void updateFilm(@Valid @RequestBody Film film) {
         if (validateFilm(film)) {
             if (films.containsKey(film.getId())) {
                 films.put(film.getId(), film);
@@ -68,21 +69,9 @@ public class FilmController {
     }
 
     private boolean validateFilm(Film film) {
-        if (film.getName().isBlank()) {
-            log.error("Название фильма не введено");
-            throw new ValidationException("Название фильма не может быть пустым");
-        } else if (film.getDescription().length() > 200) {
-            log.error("Превышена максимальная длина описания");
-            throw new ValidationException("Максимальная длина описания - 200 символов");
-        } else if (film.getDescription().isBlank()) {
-            log.error("Не введено описание фильма");
-            throw new ValidationException("Необходимо ввести описание фильма");
-        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.error("Введена неверная дата релиза");
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Введена неверная дата релиза");
             throw new ValidationException("Неверная дата релиза");
-        } else if (film.getDuration().isNegative()) {
-            log.error("Введена отрицательная продолжительность фильма");
-            throw new ValidationException("Продолжительность фильма должна быть положительной");
         } else {
             return true;
         }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.time.DurationMin;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private Integer id;
+    @NotBlank(message = "Название не должно быть пустым")
     private final String name;
+    @NotBlank(message = "Описание не должно быть пустым")
+    @Size(max = 200, message = "Длина описания не должна превышать 200 символов")
     private final String description;
     private final LocalDate releaseDate;
+    @DurationMin(message = "Продолжительность не может быть отрицательной")
     private final Duration duration;
 }
