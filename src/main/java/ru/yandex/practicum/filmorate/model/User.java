73a82b4c8a66f28f69cf6c3691e120bd569d97ed
AlexandrValter <valteralexander@yandex.ru>
2022-05-20 22:35:34 +0700

    feat: переработана архитектура, реализованы все методы API

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,9 +4,12 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
+    private Set<Integer> friends = new HashSet<>();
     private Integer id;
     private final String email;
     private final String login;
