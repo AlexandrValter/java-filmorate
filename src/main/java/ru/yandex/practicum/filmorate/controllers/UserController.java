57745ca1a6af33b3c99054ae91a7f6ddaeef8ea3
AlexandrValter <valteralexander@yandex.ru>
2022-05-21 16:02:19 +0700

    feat: завершена работа по реализации требований ТЗ 9 спринта

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -8,8 +8,8 @@ import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -29,8 +29,8 @@ public class UserController {
     }
 
     @GetMapping
-    public Map<Integer, User> getUsers() {
-        return userStorage.getUsers();
+    public Collection<User> getUsers() {
+        return userStorage.getAllUsers();
     }
 
     @PutMapping
@@ -44,22 +44,22 @@ public class UserController {
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public void addFriend (@PathVariable int id, @PathVariable int friendId){
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
         userService.addFriends(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public void deleteFriend (@PathVariable int id, @PathVariable int friendId){
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
         userService.deleteFriends(id, friendId);
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getFriends (@PathVariable int id){
+    public List<User> getFriends(@PathVariable int id) {
         return userService.getFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends (@PathVariable int id, @PathVariable int otherId){
+    public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
         return userService.getCommonFriends(id, otherId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,14 +2,17 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -18,7 +21,6 @@ public class FilmService {
 
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
-    private final List<Film> popularFilms = new ArrayList<>();
 
     @Autowired
     public FilmService(InMemoryFilmStorage filmStorage, InMemoryUserStorage userStorage) {
@@ -30,10 +32,15 @@ public class FilmService {
         if (filmStorage.getFilms().containsKey(filmId)) {
             if (userStorage.getUsers().containsKey(userId)) {
                 filmStorage.getFilm(filmId).getLikes().add(userId);
-                popularFilms.add(filmStorage.getFilm(filmId));
                 filmStorage.getFilm(filmId).setCountLikes(filmStorage.getFilm(filmId).getLikes().size());
                 log.info("Пользователь {} поставил лайк фильму {}", userStorage.getUser(userId).getEmail(), filmStorage.getFilm(filmId).getName());
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Пользователь с id %d не найден", userId));
             }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("Фильм с id %d не найден", filmId));
         }
     }
 
@@ -43,13 +50,19 @@ public class FilmService {
                 filmStorage.getFilm(filmId).getLikes().remove(userId);
                 filmStorage.getFilm(filmId).setCountLikes(filmStorage.getFilm(filmId).getLikes().size());
                 log.info("Пользователь {} удалил лайк с фильма {}", userStorage.getUser(userId).getEmail(), filmStorage.getFilm(filmId).getName());
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Пользователь с id %d не найден", userId));
             }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("Фильм с id %d не найден", filmId));
         }
     }
 
-    public List<Film> getPopularFilms(int count) {
-        return popularFilms.stream()
-                .sorted((Comparator.comparing(Film::getCountLikes).reversed()))
+    public List<Film> popularFilms(int count) {
+        return filmStorage.getAllFilms().stream()
+                .sorted(Comparator.comparingInt(Film::getCountLikes).reversed())
                 .distinct()
                 .limit(count)
                 .collect(Collectors.toList());
