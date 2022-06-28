7d73f3abb84976c3a797abcee2af57cb0cd746a7
Dmitry <dschinghis2008@yandex.ru>
2022-06-28 15:19:04 +0700

    feat: add director entity

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -3,18 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Data
 public class Director {
     private Integer id;
 
     @NotBlank
-    @Size(min = 5)
     private String name;
-
-    public Director(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
