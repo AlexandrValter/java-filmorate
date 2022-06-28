8e6b1f8ee1f8df0783ee3f3229cec74e3d0a7245
Egorovwa <egorovwa2@gmail.com>
2022-06-28 21:39:52 +0700

    test

--- src/test/java/ru/yandex/practicum/filmorate/service/recommendationHtndlerWithOutMarksImplTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/recommendationHtndlerWithOutMarksImplTest.java
@@ -3,18 +3,24 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest         // TODO: 28.06.2022  переделать под тестовую базу
+@SpringBootTest
+@AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class recommendationHtndlerWithOutMarksImplTest {
 
     private final RecommendationHtndler rh;
 
     @Test
     public void fimdRecommendation() {
+
         rh.findRecommendation(1);
     }
 }
