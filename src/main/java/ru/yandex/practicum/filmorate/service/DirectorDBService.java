4887d96ba16808710764c6e6a8dc422709dac4f0
Dmitry <dschinghis2008@yandex.ru>
2022-07-05 09:02:29 +0700

    refactor: добавить класс-сервис, добавить тесты

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorDBService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorDBService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.storage.DirectorDao;
@@ -7,6 +8,7 @@ import ru.yandex.practicum.filmorate.storage.DirectorDao;
 import java.util.List;
 
 @Service
+@Slf4j
 public class DirectorDBService implements DirectorService{
     private DirectorDao directorDao;
 
@@ -16,22 +18,27 @@ public class DirectorDBService implements DirectorService{
     }
     @Override
     public Director getDirector(Integer id){
+        log.info("запрошен режиссер id={}",id);
         return directorDao.getDirector(id);
     }
     @Override
     public List<Director> getAll(){
+        log.info("запрошены все режиссеры");
         return directorDao.getAll();
     }
     @Override
     public Director addDirector(Director director){
+        log.info("добавлен режиссер id={}",director.getId());
         return directorDao.addDirector(director);
     }
     @Override
     public Director updateDirector(Director director){
+        log.info("обновлен режиссер id={}",director.getId());
         return directorDao.updateDirector(director);
     }
     @Override
     public void removeDirector(Integer id){
         directorDao.removeDirector(id);
+        log.info("удален режиссер id={}",id);
     }
 }
